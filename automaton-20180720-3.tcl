d87b105d86cf62949a227fcfe11aaf9816f62e42
Peter Lewerin <peter.lewerin@tele2.se>
2018-07-21 21:54:17 +0200

    2154

--- automaton-20180720-3.tcl
+++ automaton-20180720-3.tcl
@@ -1,53 +1,64 @@
 
 # http://wiki.tcl-lang.org/20308
 
-catch { CState destroy }
-oo::class create CState {
-    variable transMat
-    constructor args {
-        set transMat {}
-    }
-    method newState input {
-        dict get $transMat $input
-    }
+namespace eval FSM {
+    namespace export *
 }
 
-proc Machine {name body} {
+catch { ::FSM::Runner destroy }
+oo::class create ::FSM::Runner {
+    variable current
+    method Run {start accept inputs} {
+        set current $start
+        foreach input $inputs {
+            set current [$current newState $input]
+        }
+        return [expr {$current in $accept}]
+    }
+    forward run my Run $_start $_accept
+}
+
+catch { rename ::FSM::State {} }
+proc ::FSM::State {ns name body} {
+    namespace eval ::FSM::Temp {
+        variable transMat {}
+        proc transition {input state} {
+            variable transMat
+            dict set transMat $input $state
+        }
+    }
+    namespace eval ::FSM::Temp $body
+    set o [oo::object create $ns\::$name]
+    oo::objdefine $o [format {
+        method newState input {
+            dict get {
+                %s
+            } $input
+        }
+    } $::FSM::Temp::transMat]
+    namespace delete ::FSM::Temp
+    return $o
+}
+
+catch { rename ::FSM::Machine {} }
+proc ::FSM::Machine {name body} {
     log::log d [info level 0] 
     variable _start {}
     variable _accept {}
     set ns [uplevel 1 {namespace current}]
     set o [oo::object create $ns\::$name]
-    interp alias {} ::oo::objdefine::State {} {*}[namespace code {apply {{ns name body} {
-        set o [uplevel 1 [list ::CState create $ns\::$name]]
-        oo::objdefine $o $body
-    }}}] [info object namespace $o]
-    log::log d [interp alias {} ::oo::objdefine::State]
+    interp alias {} ::oo::objdefine::State {} ::FSM::State [info object namespace $o]
     interp alias {} ::oo::objdefine::start {} set [namespace which -variable _start]
     interp alias {} ::oo::objdefine::accept {} set [namespace which -variable _accept]
     oo::objdefine $o $body
-    oo::objdefine $o variable current
+    oo::objdefine $o mixin Runner
     foreach cmd {State start accept} {
         interp alias {} ::oo::objdefine::$cmd {}
     }
-    oo::objdefine $o method step input {
-        set current [$current newState $input]
-    }
-    oo::objdefine $o method Run {start accept inputs} {
-        set current $start
-        foreach input $inputs {
-            my step $input
-        }
-        return [expr {$current in $accept}]
-    }
-    oo::objdefine $o forward run my Run $_start $_accept
+    return $o
 }
 
-
-proc ::oo::objdefine::transition {input state} {
-    set o [lindex [info level -1] 1]
-    tailcall dict set [info object namespace $o]::transMat $input $state
-}
+namespace import ::FSM::*
 
 catch { M destroy }
 
@@ -63,43 +74,3 @@ Machine M {
     start S0
     accept {S0}
 }
-
-return
-
-state label state-body
-entry action
-exit action
-transition trigger nextState ?action? ;# action is a script
-internal trigger ?action?
-choice expr0 clause0 ?exprN clauseN? elseClause ;# clause = {nextState ?action?}
-input source ?source...?
-start label ?action?
-accept label ?label...?
-
-state-body
-entry
-exit
-transition
-internal
-
-in a hierarchical SM going to state {} means returning to outer level
-orthogonal states
-Internal transitions
-self-transitions (does entry/exit action)
-Local versus external transitions
-Event deferral
-
-composite states can have a set entry state, or re-enter the last state they had
-
-event firing: exit action of current state, action of transition, entry action of new state
-
-state stack for composite states: pop off and exit, transition, entry and push
-
-
-event                               ;# different event classes generating event instances -- possibly stick to input stream
-
-???:
-FSM: create a machine instance
-state (running in FSM): create a State class from which state instances can be created
-transition: add a transition method to the State class being defined
-accept: set the accept attribute of the State class to 1
